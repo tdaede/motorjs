c4d4ba1ae8b1088800b50fbbdc2e32f58b1402c3
Thomas Daede <daede003@umn.edu>
2014-03-09 22:44:36 -0500

    Improve scope

--- scope.js
+++ scope.js
@@ -5,15 +5,22 @@ var scope = {
 
 
 scope.draw = function() {
-  this.scopeData.push([motor.state.e_theta,motor.motor_t]);
+  var data;
+  if ($("#yaxis").val() == "motor_t") {
+    data = motor.motor_t;
+  } else {
+    data = a.dot(a_ref);
+  }
   if (motor.state.e_theta < this.last_theta) {
     $.plot($("#scope-chart"),[this.scopeData], {
       xaxis: {
         min: 0,
         max: Math.PI*2
-      }
+      },
     });
     this.scopeData = [];
+  } else {
+    this.scopeData.push([motor.state.e_theta,data]);
   }
   this.last_theta = motor.state.e_theta;
 };
