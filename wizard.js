bef17ecd6130cc39ff93f2ae9d7f76376bc434ff
Thomas Daede <daede003@umn.edu>
2014-03-12 23:52:01 -0500

    Calculate flux

--- wizard.js
+++ wizard.js
@@ -1,3 +1,7 @@
+var magnetThickness = 0.005;
+var gapLength = 0.001;
+var statorThickness = 0.005;
+
 function updateMass() {
 	var outerRadius = parseFloat($('#mw-or').val());
 	var innerRadius = parseFloat($('#mw-ir').val());
@@ -7,16 +11,33 @@ function updateMass() {
 	var rotorMass = rotorVolume * steelDensity;
 	var numRotors = 2;
 	
-	var statorThickness = 0.005;
 	var copperDensity = 8960;
 	var numStators = 1;
 	var statorVolume = Math.PI*outerRadius*outerRadius*statorThickness;
 	var statorMass = statorVolume * copperDensity;
 	
-	var magnetThickness = 0.005;
 	var magnetDensity = 7500;
 	var magnetVolume = (Math.PI*outerRadius*outerRadius-Math.PI*innerRadius*innerRadius)*magnetThickness;
 	var magnetMass = magnetVolume * magnetDensity;
 	
 	$('#mw-mass').val(rotorMass*numRotors + statorMass*numStators + magnetMass*numRotors);
+}
+
+function updateFlux() {
+	var outerRadius = parseFloat($('#mw-or').val());
+	var innerRadius = parseFloat($('#mw-ir').val());
+	
+	var Br = 1.45;
+	var Hc = 891300;
+	var area = (Math.PI*outerRadius*outerRadius-Math.PI*innerRadius*innerRadius)
+	$('#mw-area').html(area);
+	var Fm = Hc*magnetThickness;
+	var Rm = magnetThickness/((Br/Hc)*area);
+	var Rg = (gapLength*2+statorThickness)/(1.257e-6*area);
+	
+	var flux = Fm*2/(Rm*2+Rg);
+	$('#mw-flux').html(flux);
+	
+	var field = flux / area;
+	$('#mw-field').html(field);
 }
